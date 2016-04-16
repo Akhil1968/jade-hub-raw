3a6236e976e03fe1c6af5e31431df09abfac451a
Akhilanand Singh <akhilanandsingh@gmail.com>
2016-04-16 16:41:08 +0530

    First commit for the code.

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var bodyparser = require('body-parser');
+var session = require('express-session');
+var routes = require('./routes/routes.js');
+
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+// ******* body-parser int
+// body-parser OLD Way
+//app.use(bodyparser());
+// body-parser New Way
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:false}));
+//body parser init done
+
+//****** session
+app.use(session({secret: "secret",  resave : true,  saveUninitialized : false}));
+ 
+app.set('view engine', 'jade');
+
+app.get('/', routes.loginPageHandler);
+app.get('/toLanding', routes.landingPageHandler);
+app.post('/toCity', routes.cityPageHandler);
+
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+	console.log('HTTP server is listening on port: ' + port);
+});
